cdecdab07c51a818b58ea9e6238bff2b1820222c
Thomaz Toyama <contato@thomazot.com.br>
2023-09-09 18:37:38 -0300

    feat: storybook install and add common view and container

---
+++ src/common/Container/Container.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Container } from './';
+
+const meta = {
+  title: 'Common/Container',
+  component: Container,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Container>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        children: <> Container </>
+    }
+};
+
