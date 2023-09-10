b20f408c6668df7abcaebb06ebdeed1b85a94e67
Thomaz Toyama <contato@thomazot.com.br>
2023-09-10 13:50:02 -0300

    feat: implemented svgs and component logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "@/components/Logo/Logo";
 import { Widget } from "@/components/Widget";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="h-screen bg-black flex items-center justify-center text-zinc-50">
       <Widget />
+      <Logo />
     </main>
   )
 }
